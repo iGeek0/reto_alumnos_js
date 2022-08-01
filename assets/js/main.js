5fff545d1f4a7c02fd7ea1c86311ec09d7ddfbac
iGeek <jesus.cardenas.d@gmail.com>
2022-07-31 22:11:51 -0500

    reto listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-/*
-Se plantea el siguiente problema
-Te pasan el siguiente codigo y te comentan que requiere
-
-1. agregar en el primer mensaje el nombre de la persona y los apellidos en el alert.
-2. Alguien creeo unas funciones de suma y resta.  Se requiere mostrar el resultado de la suma y la resta cada una en su alert respectivamente.
-3. Se requiere evitar que en consola salga la contraseña: "AgrRkeTz"
-
-
-Ejemplo de respuestas:
-1. "Su nombre es: Jesus Cardenas Dominguez"
-2. Primer Alert: "La suma es: 30" Segungo Alert "La resta es: 10"
-3. No debe salir en consola "AgrRkeTz"
-*/
-
-
-
 alert("Bienvenido a mi pagina");
 
 var nombre = prompt("Ingrese su nombre");

afd995432429d5db24fcc62a6fc9ee34b85ba777
PepeArreguin <arreguinpepe@gmail.com>
2022-08-06 10:16:02 -0500

    corte cambios JAA

---
+++ jose-angel-arreguin/assets/js/main.js
@@ -0,0 +1,21 @@
+alert("Bienvenido a mi pagina");
+
+var nombre = prompt("Ingrese su nombre");
+var apellido = prompt("Ingrese sus apellidos");
+
+alert("Su nombre es: " + nombre);
+
+var a = "20";
+var b = "10";
+var password = "AgrRkeTz";
+
+function funcionSuma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function funcionResta(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+alert("La suma es " + funcionSuma(a, b));
+alert("La resta es " + funcionResta(a, b));

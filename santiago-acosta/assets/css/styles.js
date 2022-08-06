2b000567bdf5488b8c715c67d05983f55394159c
sanacostato <saacosta.dwfsc9@ucamp.utel.edu.mx>
2022-08-06 10:19:02 -0500

    camibios

--- santiago-acosta/assets/css/styles.js
+++ santiago-acosta/assets/css/styles.js
@@ -1,1 +1,1 @@
-console.log(password);
+// console.log(password);
--- santiago-acosta/assets/js/main.js
+++ santiago-acosta/assets/js/main.js
@@ -3,10 +3,10 @@ alert("Bienvenido a mi pagina");
 var nombre = prompt("Ingrese su nombre");
 var apellido = prompt("Ingrese sus apellidos");
 
-alert("Su nombre es: " + nombre);
+alert("Su nombre es: " + nombre + apellido);
 
-var a = "20";
-var b = "10";
+var numero1 = 20;
+var numero2 = 10;
 var password = "AgrRkeTz";
 
 function funcionSuma(numero1, numero2) {
@@ -17,5 +17,5 @@ function funcionResta(numero1, numero2) {
     return numero1 - numero2;
 }
 
-alert("La suma es " + funcionSuma(a, b));
-alert("La resta es " + funcionResta(a, b));
+alert("La suma es " + funcionSuma(numero1, numero2));
+alert("La resta es " + funcionResta(numero1, numero2));

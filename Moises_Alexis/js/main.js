61dd68f52872d40ba1d64d1b6ab0f152900a8543
ToMoises <87343699+ToMoises@users.noreply.github.com>
2022-08-06 10:09:18 -0500

    hecho

---
+++ Moises_Alexis/js/main.js
@@ -0,0 +1,16 @@
+alert("Su apellido es :Torres Povis" );
+
+var a = 15;
+var b = 15;
+
+function funcionSuma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function funcionResta(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+alert("La suma es " + funcionSuma(a, b));
+alert("La resta es " + funcionResta(a, b));
+

5504c371b8c047c1baddfcca1f940c47e6a8e814
Amazingdann <alvarezvaldaniel@hotmail.com>
2022-08-16 00:08:43 -0500

    primera parte ejercicio

---
+++ daniel_alvarez/assets/js/main.js
@@ -0,0 +1,23 @@
+alert("Bienvenido a mi pagina");
+
+var nombre = prompt("Ingrese su nombre");
+var apellido = prompt("Ingrese sus apellidos");
+
+alert("Su nombre es: " + nombre);
+
+var a = "20";
+var b = "10";
+var password = "AgrRkeTz";
+
+function funcionSuma(numero1, numero2) {
+    return numero1 + numero2;
+    alert("La suma es " + funcionSuma(a, b));
+}
+
+function funcionResta(numero1, numero2) {
+    return numero1 - numero2;
+    alert("La resta es " + funcionResta(a, b));
+}
+
+
+
